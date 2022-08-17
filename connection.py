dce90738527ee119eab9276294b26933f8f54d5b
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-08-17 13:37:39 -0500

    Create connection.py

---
+++ connection.py
@@ -0,0 +1,18 @@
+###### EXERCISES ##########
+#  Exercise 1: Add a connection to the database
+###########################
+import sqlite3
+
+from flask import g
+
+db_file = "approot/app.db"
+
+def get_db():
+    connection = g.get('db', 'null')
+    if connection == 'null':
+        g.db = sqlite3.connect(db_file)
+        g.db.row_factory = sqlite3.Row
+        return g.db
+    else:
+        return connection
+
